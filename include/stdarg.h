58bbbf66a1286af0a5830a4fe8362b1720f4189c
Shixin Zeng <szeng@atronixengineering.com>
2016-09-19 15:18:02 -0400

    Avoid conflicting definition for va_list on Macs
    On Macs, va_list.h also defines va_list as (void *) for i386, which is
    also protected by _VA_LIST_H

--- include/stdarg.h
+++ include/stdarg.h
@@ -64,7 +64,12 @@ typedef struct {
 #define va_copy(dest, src) ((dest) = (src))
 
 #else /* __i386__ */
+
+/* avoid conflicting definition for va_list on Macs. */
+#if !defined(__APPLE__) || ! defined(_VA_LIST_T)
+#define _VA_LIST_T
 typedef char *va_list;
+#endif
 /* only correct for i386 */
 #define va_start(ap,last) ap = ((char *)&(last)) + ((sizeof(last)+3)&~3)
 #define va_arg(ap,type) (ap += (sizeof(type)+3)&~3, *(type *)(ap - ((sizeof(type)+3)&~3)))
